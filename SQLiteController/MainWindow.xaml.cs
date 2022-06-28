072836e7ce887a8a37b4849873dbbbb9557a7ef8
TimNekk <herew26@gmail.com>
2022-06-28 22:49:57 +0300

    Import menu added

--- SQLiteController/MainWindow.xaml.cs
+++ SQLiteController/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -13,16 +11,27 @@ namespace SQLiteController
     /// </summary>
     public partial class MainWindow
     {
-        private FileSystemInfo _dataBaseDirectory;
-        private FileSystemInfo DataBaseDirectory
+        private FileSystemInfo _dataBaseFileInfo;
+        private FileSystemInfo DataBaseFileInfo
         {
-            get => _dataBaseDirectory;
+            get => _dataBaseFileInfo;
             set
             {
-                _dataBaseDirectory = value;
-                InitializeData();
-                DataBase = new DataBase(DataBaseDirectory);
-                DataBaseDirectoryTextBox.Text = DataBaseDirectory.FullName;
+                _dataBaseFileInfo = value;
+                ResetAllListBoxes();
+                DataBase = new DataBase(DataBaseFileInfo);
+                DataBaseFileTextBox.Text = DataBaseFileInfo.FullName;
+            }
+        }
+        
+        private FileSystemInfo _importFileInfo;
+        private FileSystemInfo ImportFileInfo
+        {
+            get => _importFileInfo;
+            set
+            {
+                _importFileInfo = value;
+                ImportFileTextBox.Text = ImportFileInfo.FullName;
             }
         }
 
@@ -33,7 +42,9 @@ namespace SQLiteController
             set
             {
                 _dataBase = value;
-                SetItemsOfListBox(ExportTableListBox, DataBase.GetTablesNames());
+                var tables = DataBase.GetTablesNames();
+                SetItemsOfListBox(ExportTableListBox, tables);
+                SetItemsOfListBox(ImportTableListBox, tables);
             }
         }
         
@@ -64,64 +75,141 @@ namespace SQLiteController
                 ExportColumnLabel.Text = ExportColumn;
             }
         }
+        
+        private string _importTable;
+        private string ImportTable
+        {
+            get => _importTable;
+            set
+            {
+                _importTable = value;
+                ImportTableLabel.Text = ImportTable;
+                var columns = DataBase.GetColumnsNames(ImportTable);
+                SetItemsOfListBox(ImportColumnCheckListBox, columns);
+                SetItemsOfListBox(ImportColumnEditListBox, columns);
+            }
+        }
+        
+        private string _importColumnCheck;
+        private string ImportColumnCheck
+        {
+            get => _importColumnCheck;
+            set
+            {
+                if (value is null)
+                {
+                    ImportColumnCheckLabel.Text = "Не выбрано"; 
+                    return;
+                }
+                _importColumnCheck = value;
+                ImportColumnCheckLabel.Text = ImportColumnCheck;
+            }
+        }
+        
+        private string _importColumnEdit;
+        private string ImportColumnEdit
+        {
+            get => _importColumnEdit;
+            set
+            {
+                if (value is null)
+                {
+                    ImportColumnEditLabel.Text = "Не выбрано"; 
+                    return;
+                }
+                _importColumnEdit = value;
+                ImportColumnEditLabel.Text = ImportColumnEdit;
+            }
+        }
 
-        private const string DataBaseFileFilter = "SQLite|*.db; *.sdb; *.sqlite; *.db3; *.s3db; *.sqlite3; *.sl3|" + "Все файлы|*";
+        private const string AllFilesFilter = "Все файлы|*";
+        private const string DataBaseFilesFilter = "SQLite|*.db; *.sdb; *.sqlite; *.db3; *.s3db; *.sqlite3; *.sl3|" + AllFilesFilter;
+        private const string ImportFilesFilter = "Текстовые файлы|*.txt|" + AllFilesFilter;
         private const string NotSelected = "Не выбрано";
 
         public MainWindow()
         {
             InitializeComponent();
-            InitializeData();
-        }
-        
-        private void InitializeData()
-        {
-            _exportTable = NotSelected;
-            ExportTableLabel.Text = ExportTable;
-            ExportTableListBox.Items.Clear();
-            
-            _exportColumn = NotSelected;
-            ExportColumnLabel.Text = ExportColumn;
-            ExportColumnListBox.Items.Clear();
+            ResetAllListBoxes();
         }
 
-        private void SelectDataBaseButtonClick(object sender, RoutedEventArgs e)
+        private static void ResetListBox(ListBox listBox, TextBlock textBlock, out string variableName)
         {
+            variableName = NotSelected;
+            textBlock.Text = variableName;
+            listBox.Items.Clear();
+        }
+
+        private void ResetAllListBoxes()
+        {
+            ResetListBox(ExportTableListBox, ExportTableLabel, out _exportTable);
+            ResetListBox(ExportColumnListBox, ExportColumnLabel, out _exportColumn);
+            ResetListBox(ImportTableListBox, ImportTableLabel, out _importTable);
+            ResetListBox(ImportColumnCheckListBox, ImportColumnCheckLabel, out _importColumnCheck);
+            ResetListBox(ImportColumnEditListBox, ImportColumnEditLabel, out _importColumnEdit);
+        }
+
+        private void SelectFileButtonClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button) sender;
+            var filter = "";
+            switch (button.Name)
+            {
+                case nameof(DataBaseFileSelectButton):
+                    filter = DataBaseFilesFilter;
+                    break;
+                case nameof(ImportFileSelectButton):
+                    filter = ImportFilesFilter;
+                    break;
+            }
+            
             var dialog = new Microsoft.Win32.OpenFileDialog
             {
-                Filter = DataBaseFileFilter
+                Filter = filter
             };
 
-            var result = dialog.ShowDialog();
-
-            if (result == true)
+            if (!(dialog.ShowDialog() is true)) return;
+            
+            var file = new FileInfo(dialog.FileName);
+            switch (button.Name)
             {
-                DataBaseDirectory = new FileInfo(dialog.FileName);
+                case nameof(DataBaseFileSelectButton):
+                    DataBaseFileInfo = file;
+                    break;
+                case nameof(ImportFileSelectButton):
+                    ImportFileInfo = file;
+                    break;
             }
         }
-
-        private void ExportTableListBoxSelected(object sender, RoutedEventArgs e)
+        
+        private void ListBoxSelected(object sender, RoutedEventArgs e)
         {
             var listBox = (ListBox) sender;
             var selectedItem = (ListBoxItem) listBox.SelectedItem;
-            if (selectedItem == null) {
-                _exportTable = NotSelected;
-                ExportTableLabel.Text = ExportTable;
-                return;
-            }
-            ExportTable = selectedItem.Content.ToString();
-        }
 
-        private void ExportColumnListBoxSelected(object sender, SelectionChangedEventArgs e)
-        {
-            var listBox = (ListBox) sender;
-            var selectedItem = (ListBoxItem) listBox.SelectedItem;
-            if (selectedItem == null) {
-                _exportColumn = NotSelected;
-                ExportColumnLabel.Text = ExportColumn;
-                return;
+            switch (listBox.Name)
+            {
+                case nameof(ExportTableListBox):
+                    if (selectedItem is null) ResetListBox(listBox, ExportTableLabel, out _exportTable);
+                    else { ExportTable = selectedItem.Content.ToString(); }
+                    break;
+                case nameof(ExportColumnListBox):
+                    if (selectedItem is null) ResetListBox(listBox, ExportColumnLabel, out _exportColumn);
+                    else { ExportColumn = selectedItem.Content.ToString(); }
+                    break;
+                case nameof(ImportTableListBox):
+                    if (selectedItem is null) ResetListBox(listBox, ImportTableLabel, out _importTable);
+                    else { ImportTable = selectedItem.Content.ToString(); }
+                    break;
+                case nameof(ImportColumnCheckListBox):
+                    if (selectedItem is null) ResetListBox(listBox, ImportColumnCheckLabel, out _importColumnCheck);
+                    else { ImportColumnCheck = selectedItem.Content.ToString(); }
+                    break;
+                case nameof(ImportColumnEditListBox):
+                    if (selectedItem is null) ResetListBox(listBox, ImportColumnEditLabel, out _importColumnEdit);
+                    else { ImportColumnEdit = selectedItem.Content.ToString(); }
+                    break;
             }
-            ExportColumn = selectedItem.Content.ToString();
         }
 
         private static void SetItemsOfListBox(ListBox listBox, IEnumerable<string> items)
@@ -137,7 +225,7 @@ namespace SQLiteController
         {
             if (ExportTable == NotSelected || ExportColumn == NotSelected)
             {
-                MessageBox.Show("Не выбраны таблица или столбец");
+                MessageBox.Show("Не выбраны таблица или столбец", "Ошибка");
                 return;
             }
             
@@ -147,23 +235,40 @@ namespace SQLiteController
                 FileName = $"{ExportTable}_{ExportColumn}"
             };
             
-            var result = dialog.ShowDialog();
-
-            if (result != true) return;
+            if (dialog.ShowDialog() is false) return;
             
-            var fileInfo = new FileInfo(dialog.FileName);
-            var fileStream = fileInfo.Create();
-            var writer = new StreamWriter(fileStream);
+            using var streamWriter = new StreamWriter(dialog.FileName);
             var data = DataBase.GetData(ExportTable, ExportColumn);
             foreach (var row in data)
             {
-                writer.WriteLine(row);
+                streamWriter.WriteLine(row);
             }
-            writer.Close();
-            fileStream.Close();
 
-            
-            MessageBox.Show($"Выгружено строк: {data.Count()}", "Выгрузка завершена");
+            MessageBox.Show($"Выгружено строк: {data.Count}", "Выгрузка завершена");
         }
+
+        private void ImportButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (ImportTable == NotSelected || ImportColumnCheck == NotSelected || ImportColumnEdit == NotSelected)
+            {
+                MessageBox.Show("Не выбраны таблица или столбец для проверки или редактирования", "Ошибка");
+                return;
+            }
+            
+            var (valueIfContains, valueIfNotContains) = (ValueIfContainsTextBox.Text, ValueIfNotContainsTextBox.Text);
+            if (valueIfContains == string.Empty || valueIfNotContains == string.Empty || ImportFileInfo is null)
+            {
+                MessageBox.Show("Не выбраны файл с данными или значения \"Если в файле\" или \"Если нет\"", "Ошибка");
+                return;
+            }
+
+            using var streamReader = new StreamReader(ImportFileInfo.FullName);
+            var checkValues = streamReader.ReadToEnd().Split(new[] {'\n'}, StringSplitOptions.RemoveEmptyEntries);
+            
+            DataBase.CheckAndChangeValues(ImportTable, ImportColumnCheck, ImportColumnEdit, valueIfContains, valueIfNotContains, checkValues);
+            
+            MessageBox.Show("Импорт завершен", "Импорт завершен");
+        }
+
     }
 }
